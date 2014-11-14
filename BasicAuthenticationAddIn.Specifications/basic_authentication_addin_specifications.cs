af83b4a799c8f8818ea087cd422bdcff954419bf
Gaurav Narkhede <narkhedegs@gmail.com>
2014-11-14 16:42:46 -0500

    Added functionality to create Authorization header for Basic Authentication.

---
+++ BasicAuthenticationAddIn.Specifications/basic_authentication_addin_specifications.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Moq;
+using NSpec;
+using RestApiTester.Common;
+
+namespace RestApiTester.AddIns.Specifications
+{
+    public class basic_authentication_addin_specifications : nspec
+    {
+        private RestApiTester.AddIns.BasicAuthenticationAddIn _basicAuthenticationAddIn;
+        private string _configurationJson;
+        private Mock<IRestRequest> _restRequest;
+
+        public void when_executing_basic_authentication_addin()
+        {
+            before = () =>
+            {
+                _basicAuthenticationAddIn = new RestApiTester.AddIns.BasicAuthenticationAddIn();
+
+                _restRequest = new Mock<IRestRequest>();
+                _restRequest.Setup(request => request.Headers).Returns(new Dictionary<string, string>());
+            };
+
+            act = () => _basicAuthenticationAddIn.Execute(_configurationJson, _restRequest.Object, null, null);
+
+            context["given null or empty configuration json"] = () =>
+            {
+                before = () => _configurationJson = null;
+
+                it["should throw ArgumentNullException"] = expect<ArgumentNullException>();
+            };
+
+            context["given invalid configuration json"] = () =>
+            {
+                before = () =>
+                {
+                    _configurationJson =
+                        "{" +
+                        "\"invalidProperty1\":1," +
+                        "\"invalidProperty2\":2";
+                };
+
+                it["should throw ArgumentException"] = expect<ArgumentException>();
+            };
+
+            context["given null username"] = () =>
+            {
+                before = () =>
+                {
+                    _configurationJson =
+                        "{" +
+                        "\"username\":null," +
+                        "\"password\":\"some password\"" +
+                        "}";
+                };
+
+                it["should throw ArgumentException"] = expect<ArgumentException>();
+            };
+
+            context["given null password"] = () =>
+            {
+                before = () =>
+                {
+                    _configurationJson =
+                        "{" +
+                        "\"username\":\"some username\"," +
+                        "\"password\":null" +
+                        "}";
+                };
+
+                it["should throw ArgumentException"] = expect<ArgumentException>();
+            };
+
+            context["given valid configuration json"] = () =>
+            {
+                const string username = "some username";
+                const string password = "some password";
+                var expectedAuthorizationHeaderValue = string.Format("Basic {0}",
+                    Convert.ToBase64String(Encoding.UTF8.GetBytes(username + ":" + password)));
+
+                before = () =>
+                {
+                    _configurationJson =
+                        "{" +
+                        "\"username\":\"" + username + "\"," +
+                        "\"password\":\"" + password + "\"," +
+                        "}";
+                };
+
+                it["should populate Request Authorization Header"] =
+                    () =>
+                        _restRequest.Object.Headers.should_contain(
+                            header => header.Key == "Authorization" && header.Value == expectedAuthorizationHeaderValue);
+            };
+        }
+    }
+}
---
+++ BasicAuthenticationAddIn/BasicAuthenticationAddIn.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using Newtonsoft.Json;
+using RestApiTester.Common;
+using Environment = RestApiTester.Common.Environment;
+
+namespace RestApiTester.AddIns
+{
+    public class BasicAuthenticationAddIn : IBeforeRequestRunAddIn
+    {
+        public void Execute(
+            string configurationJson, IRestRequest request, Environment environment, IRestClient restClient)
+        {
+            if (string.IsNullOrEmpty(configurationJson))
+                throw new ArgumentNullException("configurationJson",
+                    "Please provide configuration for " + GetType().Name + ".");
+
+            try
+            {
+                var configuration =
+                    JsonConvert.DeserializeObject<BasicAuthenticationAddInConfiguration>(configurationJson);
+
+                if (configuration.Username == null)
+                    throw new ArgumentException("username cannot be null.", "configurationJson");
+                if (configuration.Password == null)
+                    throw new ArgumentException("password cannot be null.", "configurationJson");
+
+                var authorizationHeaderValue = string.Format("Basic {0}",
+                    Convert.ToBase64String(Encoding.UTF8.GetBytes(configuration.Username + ":" + configuration.Password)));
+
+                request.Headers.Add("Authorization",authorizationHeaderValue);
+            }
+            catch (JsonSerializationException jsonSerializationException)
+            {
+                throw new ArgumentException(
+                    "Invalid configuration json. Please provide a valid configuration for " + GetType().Name + ".",
+                    "configurationJson", jsonSerializationException);
+            }
+        }
+    }
+}
