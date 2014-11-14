8487e34074a36042b27a4edae5306d13015e4bf6
Gaurav Narkhede <narkhedegs@gmail.com>
2014-11-14 14:53:07 -0500

    Initial Commit.

---
+++ RestApiTester.AddIns.BasicAuthenticationAddIn/BasicAuthenticationAddInConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace RestApiTester.AddIns
+{
+    internal class BasicAuthenticationAddInConfiguration
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ RestApiTester.AddIns.BasicAuthenticationAddIn/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RestApiTester.AddIns.BasicAuthenticationAddIn")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("GSN")]
+[assembly: AssemblyProduct("RestApiTester.AddIns.BasicAuthenticationAddIn")]
+[assembly: AssemblyCopyright("Copyright © 2014 Gaurav Narkhede<narkhedegs@gmail.com>")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0c540f54-4e5c-428d-868c-ad62262d64c0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
